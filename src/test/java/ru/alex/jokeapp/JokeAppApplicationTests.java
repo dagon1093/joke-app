80dc3482ae4df928a563527be2befd8da17d2bf4
dagon1093 <P0werofgood>
2021-08-21 16:57:49 +0300

    init commit. close #1

---
+++ src/test/java/ru/alex/jokeapp/JokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alex.jokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
